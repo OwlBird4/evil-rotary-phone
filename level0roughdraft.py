b370d0944c21d00f669c1ac6fea306c4fbb66bea
OwlBird4 <michael@shipman.ws>
2017-08-31 15:42:52 -0400

    added image initialization
    I changed the marioinair to mario_in_air to stay consistent with names of images in images branch.

--- level0roughdraft.py
+++ level0roughdraft.py
@@ -1,11 +1,10 @@
-while gameOver == False :
-    pygame.display.update()
-
-
+    Windows-10-desktop = Image("Windows-10-desktop.png",screenWidth,screenHeight,True);
+    mario_standing = Image("mario_standing.png",150,150,True);
+    mario_in_air = Image("mario_in_air.png",150,150,True);
     if level == 0 :
         # Intro Screen Code
         windows.display()
-        marioair.display()
+        mario_standing.display()
     
  
         pygame.display.update()
