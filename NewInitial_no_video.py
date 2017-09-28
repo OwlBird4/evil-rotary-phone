85466b6cd1fb36ce8b99243354b1182379d5c4e9
Bawllp <31519150+Bawllp@users.noreply.github.com>
2017-09-28 11:11:58 -0400

    Update NewInitial_no_video.py

--- NewInitial_no_video.py
+++ NewInitial_no_video.py
@@ -1,11 +1,6 @@
 import pygame,sys,random,time
 import math
 import time
-#import moviepy
-#from moviepy.editor import *
-#import os
-#import imageio
-#imageio.plugins.ffmpeg.download()
 
          
 class Image:
@@ -93,6 +88,7 @@ RED = (255, 0, 0)
 BLUE = (0,0,255)
 
 shipsremain = 8
+outofbounds = 350
 
 
 #initialize some variables
@@ -101,7 +97,6 @@ jumpmode=False
 run_once=False
 run_once_2=False
 run_once_3=False
-run_once_4=False
 level = 0
 score = 0
 gameOver = False
@@ -120,8 +115,6 @@ pygame.display.set_caption("haxxor")
 # load images
 Windows_10_desktop = Image("Windows-10-desktop.png",screenWidth,screenHeight,True);
 mario_standing = Image("mario_standing.png",75,75,True);
-mario_standing_2 = Image("mario_standing.png",75,75,True);
-mario_in_air = Image("mario_in_air.png",75,75,True);
 galaga_icon_locked = Image("galaga_icon_locked.png",150,150,True);
 galaga_icon_unlocked = Image("galaga_icon_unlocked.png",150,150,True);
 ship_fighter = Image("ship_fighter.png",100,100,True);
@@ -135,60 +128,50 @@ alien_ship5 =Image("alien_small.png",50,50,True);
 alien_ship6 =Image("alien_small.png",50,50,True);
 alien_ship7 =Image("alien_small.png",50,50,True);
 alien_ship8 =Image("alien_small.png",50,50,True);
-cat_mouse_locked =Image("cat_mouse_locked.png",150,150,True);
-cat_mouse_unlocked =Image("cat_mouse_unlocked.png",150,150,True);
-tower_defense_icon_locked = Image("tower_defense_icon_locked.png",100,100,True);
-tower_defense_icon_unlocked = Image("tower_defense_icon_unlocked.png",100,100,True);
+galaga_icon_locked2 = Image("galaga_icon_locked.png",150,150,True);
+mario_standing2 = Image("mario_standing.png",75,75,True);
+
+
+# load sounds
+laser = pygame.mixer.Sound("Sounds//BLOWBACK.WAV")
+galagamusic = pygame.mixer.Sound("Sounds//galagamusic.wav")
+applause = pygame.mixer.Sound("Sounds//APPLAUSE.wav")
+explosion = pygame.mixer.Sound("Sounds//UNKNOWN.WAV")
+motherboard_galaga = pygame.mixer.Sound("Sounds//motherboard_galaga.wav")
 
 while gameOver == False :
     pygame.display.update()
 
     if jumpmode==True :
-           mario_standing.x+= 10
-           mario_standing.y-= 10
-           mario_in_air.x = mario_standing.x
-           mario_in_air.y = mario_standing.y
-           mario_standing = mario_in_air
-           reps+=1
-           if reps == 10 :
-               jumpmode=False
-               reps=0
+            mario_standing.x+= 10
+            mario_standing.y-= 10
+            reps+=1
+            if reps == 10 :
+                jumpmode=False
+                reps=0
 
     mario_standing.y+= 5
 
     if mario_standing.y >= 600-mario_standing.height :
-        mario_standing_2.x = mario_standing.x
-        mario_standing_2.y = mario_standing.y
-        mario_standing = mario_standing_2
         mario_standing.y = 600-mario_standing.height
     
     if level == 0 :
-#        if run_once_4 == False :
-            # Video Code
-#            os.environ["SDL_VIDEO_CENTERED"] = "1"
-#            clip = VideoFileClip('test.mp4')
-#            clip.preview()
-#            screenWidth = 1150
-#            screenHeight =  600
-#            screen = pygame.display.set_mode([screenWidth,screenHeight])
-#            pygame.init()
-#            run_once_4 = True
-
         # Intro Screen Code
         galaga_icon_locked.x=900
         galaga_icon_locked.y=468
-        cat_mouse_locked.x=600
-        cat_mouse_locked.y=468
-        tower_defense_icon_locked.x=300
-        tower_defense_icon_locked.y=510
         Windows_10_desktop.display()
         galaga_icon_locked.display()
-        cat_mouse_locked.display()
-        tower_defense_icon_locked.display()
         text = myFont48.render("Welcome Haxxor!",True,RED)
+        text = myFont24.render("Created By Pleasant Ballenger and Michael Shipman",True,BLACK)
         screen.blit(text,[300,100])   
         mario_standing.display()
         pygame.display.update()
+
+        if imagesCollide(mario_standing,galaga_icon_locked):
+            text = myFont24.render("Press Enter!",True,WHITE)
+            screen.blit(text,[900,368])
+
+                    
         
         if run_once==False :
             enter = False
@@ -198,77 +181,24 @@ while gameOver == False :
         if enter == True and imagesCollide(mario_standing,galaga_icon_locked) and level == 0 :
             x = galaga_icon_locked.x
             y = galaga_icon_locked.y
-            galaga_icon_locked.x = x
-            galaga_icon_locked.y = y
-            Windows_10_desktop.display()
             galaga_icon_locked = Image("galaga_icon_unlocked.png",150,150,True);
             galaga_icon_locked.x = x
             galaga_icon_locked.y = y
-            galaga_icon_locked.display();
             galaga_icon_locked.display()
-            cat_mouse_locked.display()
-            tower_defense_icon_locked.display();
-            mario_standing.display()
-            galaga_icon_locked.x = x
-            galaga_icon_locked.y = y
-            text = myFont48.render("Welcome Haxxor!",True,RED)
-            screen.blit(text,[300,100])
             pygame.display.update()
+            Windows_10_desktop.display()
             pygame.time.delay(1000)
+            galaga_icon_locked = Image("galaga_icon_unlocked.png",150,150,True);
+            galaga_icon_locked.display();
+            pygame.display.update()
             level = 1
 
-        if enter == True and imagesCollide(mario_standing,cat_mouse_locked) and level == 0 :
-            x = cat_mouse_locked.x
-            y = cat_mouse_locked.y
-            cat_mouse_locked.x = x
-            cat_mouse_locked.y = y
-            Windows_10_desktop.display()
-            cat_mouse_locked = Image("cat_mouse_unlocked.png",150,150,True);
-            cat_mouse_locked.x = x
-            cat_mouse_locked.y = y
-            cat_mouse_locked.display();
-            galaga_icon_locked.display()
-            cat_mouse_locked.display()
-            tower_defense_icon_locked.display();
-            mario_standing.display()
-            cat_mouse_locked.x = x
-            cat_mouse_locked.y = y
-            text = myFont48.render("Welcome Haxxor!",True,RED)
-            screen.blit(text,[300,100])
-            pygame.display.update()
-            pygame.time.delay(1000)
-            level = 2
-
-        if enter == True and imagesCollide(mario_standing,tower_defense_icon_locked) and level == 0 :
-            x = tower_defense_icon_locked.x
-            y = tower_defense_icon_locked.y
-            tower_defense_icon_locked.x = x
-            tower_defense_icon_locked.y = y
-            Windows_10_desktop.display()
-            tower_defense_icon_locked = Image("tower_defense_icon_unlocked.png",150,150,True);
-            tower_defense_icon_locked.x = x
-            tower_defense_icon_locked.y = y
-            tower_defense_icon_locked.display();
-            galaga_icon_locked.display()
-            cat_mouse_locked.display()
-            tower_defense_icon_locked.display();
-            mario_standing.display()
-            tower_defense_icon_locked.x = x
-            tower_defense_icon_locked.y = y
-            text = myFont48.render("Welcome Haxxor!",True,RED)
-            screen.blit(text,[300,100])
-            pygame.display.update()
-            pygame.time.delay(1000)
-            level = 3
-
-
         enter = False
-
 
 #-----------------#LEVEL 1 CODE
     
     if level == 1:
-
+        
         space_back.display()
         if run_once_2 == False :
             alien_ship.x = 900
@@ -287,6 +217,7 @@ while gameOver == False :
             alien_ship7.y = 518
             alien_ship8.x = 600
             alien_ship8.y = 518
+            motherboard_galaga.play()
             run_once_2 = True
         ship_fighter.display()
         alien_ship.display()
@@ -298,52 +229,129 @@ while gameOver == False :
         alien_ship7.display()
         alien_ship8.display()
 
+
         text = myFont24.render("Ships That Remain:"+str(shipsremain),True,WHITE)
         screen.blit(text,[600,0])
 
+        text = myFont24.render("Out of Bounds Timer:"+str(outofbounds),True,RED)
+        screen.blit(text,[200,0])
+
         if showLaser == True:
             laser_bullet.display()
-            laser_bullet.y+=5
-
+            laser_bullet.y+=12
+            
         if imagesCollide(laser_bullet,alien_ship) :
-            alien_ship.x = 3100
-            alien_ship.y = 3100
+            alien_ship.x = 200
+            alien_ship.y = 10000
             shipsremain -= 1
+            explosion.play()
 
         if imagesCollide(laser_bullet,alien_ship2) :
-            alien_ship2.x = 3100
-            alien_ship2.y = 3100
+            alien_ship2.x = 200
+            alien_ship2.y = 10000
             shipsremain -= 1
+            explosion.play()
 
         if imagesCollide(laser_bullet,alien_ship3) :
-            alien_ship3.x = 3100
-            alien_ship3.y = 3100
+            alien_ship3.x = 200
+            alien_ship3.y = 10000
             shipsremain -= 1
+            explosion.play()
 
         if imagesCollide(laser_bullet,alien_ship4) :
-            alien_ship4.x = 3100
-            alien_ship4.y = 3100
+            alien_ship4.x = 200
+            alien_ship4.y = 10000
             shipsremain -= 1
+            explosion.play()
 
         if imagesCollide(laser_bullet,alien_ship5) :
-            alien_ship5.x = 3100
-            alien_ship5.y = 3100
+            alien_ship5.x = 200
+            alien_ship5.y = 10000
             shipsremain -= 1
+            explosion.play()
 
         if imagesCollide(laser_bullet,alien_ship6) :
-            alien_ship6.x = 3100
-            alien_ship6.y = 3100
+            alien_ship6.x = 200
+            alien_ship6.y = 10000
             shipsremain -= 1
+            explosion.play()
 
         if imagesCollide(laser_bullet,alien_ship7) :
-            alien_ship7.x = 3100
-            alien_ship7.y = 3100
+            alien_ship7.x = 200
+            alien_ship7.y = 10000
             shipsremain -= 1
+            explosion.play()
 
         if imagesCollide(laser_bullet,alien_ship8) :
-            alien_ship8.x = 3100
-            alien_ship8.y = 3100
+            alien_ship8.x = 200
+            alien_ship8.y = 10000
             shipsremain -= 1
+            explosion.play()
+
+        if alien_ship.x > 1150:
+            outofbounds -= 1
+
+        if alien_ship2.x > 1150:
+            outofbounds -= 1
+
+        if alien_ship3.x > 1150:
+            outofbounds -= 1
+
+        if alien_ship4.x > 1150:
+            outofbounds -= 1
+
+        if alien_ship5.x > 1150:
+            outofbounds -= 1
+
+        if alien_ship6.x > 1150:
+            outofbounds -= 1
+
+        if alien_ship7.x > 1150:
+            outofbounds -= 1
+
+        if alien_ship8.x > 1150:
+            outofbounds -= 1
+
+        if alien_ship.x < -100:
+            outofbounds -= 1
+
+        if alien_ship2.x < -100:
+            outofbounds -= 1
+
+        if alien_ship3.x < -100:
+            outofbounds -= 1
+
+        if alien_ship4.x < -100:
+            outofbounds -= 1
+
+        if alien_ship5.x < -100:
+            outofbounds -= 1
+
+        if alien_ship6.x < -100:
+            outofbounds -= 1
+
+        if alien_ship7.x < -100:
+            outofbounds -= 1
+
+        if alien_ship8.x < -100:
+            outofbounds -= 1
+
+        if ship_fighter.x > 1150:
+            ship_fighter.x = 1
+            ship_fighter.y = 1
+
+        if ship_fighter.x < 0:
+            ship_fighter.x = 1100
+            ship_fighter.y = 1
+
+
+
+
+        if shipsremain <1 and level == 1:
+            level=2
+
+        if outofbounds <1 and level == 1:
+            level=2
             
 
             
@@ -354,15 +362,47 @@ while gameOver == False :
 #-----------------#LEVEL 2 CODE
     
     if level == 2:
-        screen.fill(RED)
-        mario_standing.display()
-
-
-#-----------------#LEVEL 3 CODE
+        # Intro Screen Code
+        galaga_icon_locked2.x=900
+        galaga_icon_locked2.y=468
+        Windows_10_desktop.display()
+        galaga_icon_locked2.display()
+        text = myFont48.render("Welcome Haxxor!",True,RED)
+        text = myFont24.render("Created By Pleasant Ballenger and Michael Shipman",True,BLACK)
+        screen.blit(text,[300,100])   
+        mario_standing2.display()
+        pygame.display.update()
         
-    if level == 3:
-        screen.fill(GREEN)
-        mario_standing.display()
+        if run_once==False :
+            enter = False
+            run_once = True
+
+
+#-----------------------#LEVEL 3 CODE
+
+
+
+
+
+
+
+
+
+
+#-----------------------#LEVEL 4 CODE
+
+
+
+
+
+
+#----------------------------#BOSS LEVEL CODE---------------------------------------
+
+
+
+
+
+
   
         
    
@@ -440,11 +480,13 @@ while gameOver == False :
             alien_ship7.x-=5
             alien_ship8.x-=5
 
-
-        if keys_pressed[pygame.K_SPACE]:
+        if keys_pressed[pygame.K_SPACE] and level == 1:
             showLaser = True
             laser_bullet.x = ship_fighter.x
             laser_bullet.y = ship_fighter.y
+            laser.play()
+            
+            
 
     
             
@@ -461,3 +503,8 @@ while gameOver == False :
 
 pygame.quit() #do not move 
 sys.exit() #do not move 
+
+
+ 
+
+ 
